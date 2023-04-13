7c1874d20334fcfb44ccaae10ce91e9a2bf36bd8
SoldierCantKill <thatonestreamerninja@gmail.com>
2023-04-13 05:16:59 -0400

    changed writer and added audio class

---
+++ scripts/autoload/audio.gd
@@ -0,0 +1,95 @@
+extends Node
+
+var AUDIO_PATH: String = "res://Audio/" #CANNOT USE UIDS FOR THIS!!!!
+
+var references = {}
+var global_volume = .05
+var volume_store = global_volume
+var music : Node = null
+
+var current_sounds = []
+
+func _enter_tree() -> void:
+	var dir: DirAccess = DirAccess.open("res://Audio")
+	if dir:
+		dir.list_dir_begin()
+		var dir_name: String = dir.get_next()
+		
+		while dir_name != "": if dir.current_is_dir():
+			var group: Node = Node.new()
+			group.name = dir_name
+			add_child(group)
+			
+			import_audio(str("res://Audio/" + dir_name), group)
+			#print(dir_name)
+			dir_name = dir.get_next()
+
+func import_audio(dir_name: String, group: Node):
+	var file: DirAccess = DirAccess.open(dir_name)
+	var group_name: String = str(group.name)
+	if file:
+		file.list_dir_begin()
+		var file_name: String = file.get_next()
+		while file_name != "": if !file.current_is_dir():
+			var base_name: String = file_name.get_basename().get_basename() #Only works for .import files
+			if file_name.get_extension() == "import":
+				file_name = file_name.split(".import")[0]
+				var node: Node = null
+				var node_name: String = base_name
+				if base_name.ends_with("_2d"): node = AudioStreamPlayer2D.new(); node_name = node_name.trim_suffix("_2d")
+				elif base_name.ends_with("_3d"): node = AudioStreamPlayer3D.new(); node_name = node_name.trim_suffix("_3d")
+				else: node = AudioStreamPlayer.new()
+				node.name = node_name
+				node.stream = load(dir_name.path_join(file_name))
+				group.add_child(node)
+				references[group_name.path_join(node_name)] = node
+				
+			file_name = file.get_next()
+
+func play(sound : String, volume : float = global_volume, pitch : float = 1) -> Node:
+	var n: Node = references[sound]
+	current_sounds.append(n)
+	n.finished.connect(func x(): current_sounds.erase(n))
+	n.volume_db = linear_to_db(volume)
+	n.pitch_scale = pitch
+	n.play()
+	return n
+
+func stop_sound(sound : String):
+	references[sound].stop()
+	
+func stop_all_sounds():
+	for i in current_sounds:
+		i.stop()
+
+#This function exists for you can easily manage music.
+func play_music(sound : String, volume : float = global_volume, pitch : float = 1) -> Node:
+	var n: Node = references[sound]
+	n.volume_db = linear_to_db(volume)
+	n.pitch_scale = pitch
+	n.play()
+	music = n
+	return n
+
+func pause_music():
+	if(is_instance_valid(music)):
+		music.stream_paused = true
+
+func resume_music():
+	if(is_instance_valid(music)):
+		music.play()
+
+func stop_music():
+	if(is_instance_valid(music)):
+		music.stop()
+
+func mute_volume():
+	volume_store = global_volume
+	global_volume = 0.000000001
+	for i in current_sounds:
+		i.volume = linear_to_db(global_volume)
+
+func unmute_volume():
+	global_volume = volume_store
+	for i in current_sounds:
+		i.volume_db = linear_to_db(global_volume)
--- scripts/global/writer.gd
+++ scripts/global/writer.gd
@@ -1,90 +1,224 @@
 extends RichTextLabel
-class_name writer
+
+
+signal text_index_finished
+var can_text_index_finished : bool = false
+signal text_finished
+var can_text_finished : bool = false
+signal text_close
+signal text_next
+
+var active : bool = false
+var is_writing : bool = false
+
+var overworld_border : CanvasItem = null
+var character_face : CanvasItem = null
+
+var text_array : Array
+var unformattedtext : String
+var text_index : int = 0
+var sound_index : int = 0
+var char_index : int = -1
+var font : String
+var sound : String
+var speed_timer : float = 0
+var speed : float
+var temp_speed : float
+
+var continue_enabled : bool = true # Z
+var skip_enabled : bool = true # X
+
+var dialouge_mode : bool = false #This still works if continute and skip are enabled... BEWARE!
+var dialouge_timer : float = 3
 
 var fonts = {
-	"Mono" : "res://assets/fonts/main_mono.ttf",
-	"Sans" : "res://assets/fonts/sans.ttf",
-	"DT_Sans" : "res://assets/fonts/main.ttf",
+	"Mono" : "res://Fonts/main_mono.ttf",
+	"Sans" : "res://Fonts/sans.ttf",
+	"DT_Sans" : "res://Fonts/main.ttf",
+	"Papyrus" : "res://Fonts/papyrus-font-undertale.ttf",
 	}
 
 var sounds = {
 	"None" : [""],
 	"Mono1" : ["Characters/SND_TXT1"],
 	"Mono2" : ["Characters/SND_TXT2"],
-	"Sans" : ["Characters/sans"]
+	"Sans" : ["Characters/sans"],
+	"Toriel" : ["Characters/toriel"],
+	"Papyrus" : ["Characters/papyrus"],
+	"Flowey2" : ["Characters/snd_floweytalk2"]
 	}
 
-var text_array : Array = []
-var array_index : int = 0
-var current_text : String = ""
-var unfiltered_text : String = "="
-var index : int = 0
-var speed : float = 0.033333
-var timer : float = 0
-var is_typing : bool = true
+var character_faces = {
+	"Toriel" : {"Speak1" : "res://Animations/Talking/Toriel/Speak1.tres"}
+	}
 
-func _ready():
-	new_text(["/(font=Sans)yyyyyyyyyyyyyyyyyyyyyyy/(speed=2)[font=res://assets/fonts/sans.ttf][color=red]ddddddddddddddddd"])
+func _init()  -> void:
+	bbcode_enabled = true
 
-func new_text(text : Array):
+func _ready()  -> void:
+	text_index_finished.connect(SpriteIdle)
+	var tremble_resource = RichTextEffect.new()
+	tremble_resource.set_script(load("res://Scripts/Resources/tremble.gd"))
+	custom_effects.append(tremble_resource)
+
+func SetOptions(continue_enabled : bool, skip_enabled : bool, dialouge_mode : bool)  -> void:
+	self.continue_enabled = continue_enabled #Z
+	self.skip_enabled = skip_enabled #X
+	self.dialouge_mode = dialouge_mode #Dusttrust animation cutscene #5559
+
+#You'll need to make it unskippable and uncontinuable using SetOptions().
+func MessageText(text : Array, sound : String = "Mono1", font : String = "Mono", font_size : int = 31, speed : float = 0.033333)  -> void:
+	self.text = ""
 	text_array = text
-	array_index = 0
-	next()
-	
-signal func_done
+	self.speed = speed
+	self.sound = sound
+	add_theme_font_override("normal_font", load(fonts[font]))
+	add_theme_font_size_override("normal_font_size", font_size)
+	text_index = -1
+	Reset()
 
-func next():
-	unfiltered_text = text_array[array_index]
-	current_text = unfiltered_text
+func OWText(text : Array, pos : String = "Bottom", sound : String = "Mono1", font : String = "Mono", font_size : int = 31, speed : float = 0.033333)  -> void:
+	if(is_instance_valid(character_face)):
+		character_face.sprite_frames = null
+	BoxToggle(true, pos)
+	self.text = ""
+	text_array = text
+	self.speed = speed
+	self.sound = sound
+	add_theme_font_override("normal_font", load(fonts[font]))
+	add_theme_font_size_override("normal_font_size", font_size)
+	text_index = -1
+	Reset()
 	
-	var x = func():
-		var start = current_text.findn("/(")
-		var finish = current_text.findn(")") - start + 1
-		print("FINISH ", start," ",finish)
-		var remove : String = current_text.substr(start, finish)
-		print("REMOVED ",remove)
-		current_text = current_text.replace(remove,"")
-		print("NEW ",current_text)
-		func_done.emit()
-	while(true):
-		if(current_text.findn("/(") != -1 || current_text.findn(")") != -1):
-			x.call()
-			await func_done
-		
+func CharacterText(text : Array, character : String, expression : String, pos : String = "Bottom", sound : String = "Mono1", font : String = "Mono", font_size = 31, speed : float = 0.033333)  -> void:
+	if(is_instance_valid(character_face)):
+		character_face.sprite_frames = load(str(character_faces[character][expression]))
+	BoxToggle(true, pos)
+	self.text = ""
+	text_array = text
+	self.speed = speed
+	self.sound = sound
+	add_theme_font_override("normal_font", load(fonts[font]))
+	add_theme_font_size_override("normal_font_size", font_size)
+	text_index = -1
+	Reset()
+
+func BoxToggle(visible : bool, pos : String = "Bottom"):
+	if(is_instance_valid(overworld_border) && is_instance_valid(character_face)):
+		overworld_border.visible = visible
+		overworld_border.global_position = Vector2(35, 325) if pos == "Bottom" else Vector2(35, 30) if pos == "Top" else Vector2(0, 0)
+		if(!character_face.sprite_frames):
+			position = Vector2(27.5,20)
 		else:
-			print("DONE")
-			break
-	text = current_text
-	print(text)
-	is_typing = true
+			position = Vector2(143,20)
 
-func type():
-	var filtered_text_block = RichTextLabel.new()
-	filtered_text_block.text = unfiltered_text
-	filtered_text_block.bbcode_enabled = true
-	var filtered_text = filtered_text_block.get_parsed_text()
-	#print(filtered_text)
-	if(filtered_text[index] == "/" && index < filtered_text.length() - 1):
-		if(filtered_text[index + 1] == "("):
-			var right : int = filtered_text.findn(")",index)
-			var parameter : String = filtered_text.substr(index,right - index + 1)
-			if(parameter.contains("speed")):
-				index += 1
-				#print(int(parameter.substr(6)))
-				speed = int(parameter.substr(6))
-				return
-				return
+func SpriteTalk():
+	if(is_instance_valid(character_face)):
+		character_face.animation = "speaking"
+		character_face.play()
+
+func SpriteIdle():
+	if(is_instance_valid(character_face)):
+		character_face.animation = "idle"
+		character_face.play()
+
+func _process(delta) -> void:
+	if(is_writing):
+		speed_timer += (temp_speed * (delta / temp_speed))
+		if speed_timer >= temp_speed:
+			speed_timer = 0
+			temp_speed = speed
+			Write()
+		if(Input.is_action_just_pressed("exit") && skip_enabled && text != ""):
+			
+			is_writing = false
+			visible_characters = -1
+			
+	if(text_array != [] && active):
+		if(Input.is_action_just_pressed("confirm") && continue_enabled):
+			if(visible_characters == get_parsed_text().length() || visible_characters == -1):
+				if(dialouge_mode):
+					await get_tree().create_timer(dialouge_timer).timeout
+				Reset()
 				
+		if(visible_characters == get_parsed_text().length() || visible_characters == -1):
+			if(can_text_index_finished):
+				can_text_index_finished = false
+				text_index_finished.emit()
+				if(dialouge_mode):
+					await get_tree().create_timer(dialouge_timer).timeout
+					Reset()
+		if(visible_characters == get_parsed_text().length() || visible_characters == -1):
+			if(text == text_array[text_array.size() - 1].replace("&", "") && can_text_finished):
+				can_text_finished = false
+				text_finished.emit()
+				if(dialouge_mode):
+					await get_tree().create_timer(dialouge_timer).timeout
+
+func Write() -> void:
+	char_index += 1
+	if char_index >= unformattedtext.length() - 1:
+		is_writing = false
+	if !(
+			unformattedtext[char_index] == "" or
+			unformattedtext[char_index] == " " or
+			unformattedtext[char_index] == "^" or
+			unformattedtext[char_index] == "!" or
+			unformattedtext[char_index] == "?" or
+			unformattedtext[char_index] == "," or
+			unformattedtext[char_index] == ":" or
+			unformattedtext[char_index] == "/" or
+			unformattedtext[char_index] == "\\" or
+			unformattedtext[char_index] == "|" or
+			unformattedtext[char_index] == "*" or
+			unformattedtext[char_index] == "~"
+		):
+		if (unformattedtext[char_index] == '&'):
+			temp_speed = .25
+			return
+		
+		if(IsValidChar(char_index - 1)):
+			if(unformattedtext[char_index - 1] != '~'):
+				if(sounds[sound][0] != ""):
+					audio.play(sounds[sound][0])
+			else:
+				return
+		else:
+			if(sounds[sound][0] != ""):
+					audio.play(sounds[sound][0])
+		#sound_index = wrapi(sound_index + 1, 0, sounds[sound].size())
 	visible_characters += 1
-	index += 1
 
+func IsValidChar(num : int) -> bool:
+	return num > -1 && num < unformattedtext.length()
 
-func _process(delta):
-	if(is_typing):
-		timer += delta
-		#print(timer)
-		if(timer >= speed):
-			timer = 0
-			type()
-			if(index == unfiltered_text.length()):
-				is_typing = false
+func Clear()  -> void: #Can be called by the developer
+	active = false
+	BoxToggle(false)
+	is_writing = false
+	text_index = 0
+	sound_index = 0
+	text = ""
+	text_array = []
+
+func Reset()  -> void: #Should only be called via this script.
+	await get_tree().create_timer(get_process_delta_time()).timeout
+	is_writing = true
+	text_index += 1
+	sound_index = 0
+	char_index = -1
+	visible_characters = 0
+	temp_speed = speed
+	if text_array.size() > text_index:
+		text = text_array[text_index]
+		unformattedtext = get_parsed_text()
+		text = text.replace("&", "")
+		text = text.replace("~", "")
+		can_text_index_finished = true
+		can_text_finished = true
+		active = true
+		SpriteTalk()
+		text_next.emit()
+	else:
+		Clear()
+		text_close.emit()
