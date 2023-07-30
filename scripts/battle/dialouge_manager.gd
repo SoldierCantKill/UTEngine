586908c412aa7abe36d0d87d4824c4a675fdc58a
SoldierCantKill <thatonestreamerninja@gmail.com>
2023-07-30 03:11:41 -0400

    Added sans
    instead of the fight button you now fight sans

--- scripts/battle/dialouge_manager.gd
+++ scripts/battle/dialouge_manager.gd
@@ -7,9 +7,4 @@ func _ready():
 	pass
 
 func start():
-	var enemy = vars.enemies.get_child(0)
-	enemy.speech_bubble.visible = true
-	enemy.speech_writer.writer_text = "(font:mono)(sound:mono1)how are you?(pc)"
-	await enemy.speech_writer.done
-	enemy.speech_bubble.visible = false
 	done.emit()
